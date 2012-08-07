9134e4edd36a6e7e9b1c7d1d9de230e58edc0d54
Seamus Abshere <seamus@abshere.net>
2012-08-06 20:28:32 -0500

    test input [sql] and output

--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -1,15 +1,18 @@
 require 'spec_helper'
 require 'yaml'
 
-describe 'Parsing examples' do
-  Dir[File.expand_path('../parsing_examples/*.yml', __FILE__)].each do |path|
+describe 'Examples' do
+  Dir[File.expand_path('../examples/*.yml', __FILE__)].each do |path|
     e = YAML.load_file(path)
     describe e.fetch('name', path) do
       before do
-        @c = CreateTable.new(e['sql'])
+        @c = CreateTable.new(e['input'])
+      end
+      it "generates correct output" do
+        assert_same_sql @c.to_sql, e['output']
       end
       e.each do |k, ref|
-        next if ['name', 'sql'].include?(k)
+        next if ['name', 'input', 'output'].include?(k)
         it "checks #{k}" do
           calc = if k.include?('.')
             eval(%{@c.#{k}})
